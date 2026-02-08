155787b13704b113f4f1bab83daf961d99ae0313
LakshayJain1 <lakshayjain326@gmail.com>
2026-02-08 14:37:36 +0530

    fixed bugs

--- api/notion.ts
+++ api/notion.ts
@@ -88,7 +88,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
                             },
                         ],
                     },
-                    "Submitted at": {
+                    "Submitted At": {
                         rich_text: [
                             {
                                 text: {
